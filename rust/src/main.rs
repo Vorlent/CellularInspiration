6628da49b354652922b4e5766fb1723f2d7b0648
Vorlent <vorlent@web.de>
2020-07-09 23:59:49 +0200

    Formatting

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,24 +7,24 @@ fn main() {
 
     let inner_grid = grid.slice(s![1..-1, 1..-1]);
     inner_grid.indexed_iter().for_each(|((x,y), v)| {
-      let outer_x = x + 1;
-      let outer_y = y + 1;
-      println!("cell {},{} has value {}", outer_x, outer_y, v);
-      let neighbours = grid.slice(s![(outer_x-1)..=(outer_x+1), (outer_y-1)..=(outer_y+1)]);
-      let living_neighbours = neighbours.indexed_iter().fold(0, |acc, ((n_x,n_y), n_v)| {
-        if n_x != 1 && n_y != 1 && grid[[n_x,n_y]] != 0 {
-            acc + 1
-        } else {
-            acc
+        let outer_x = x + 1;
+        let outer_y = y + 1;
+        println!("cell {},{} has value {}", outer_x, outer_y, v);
+        let neighbours = grid.slice(s![(outer_x-1)..=(outer_x+1), (outer_y-1)..=(outer_y+1)]);
+        let living_neighbours = neighbours.indexed_iter().fold(0, |acc, ((n_x,n_y), n_v)| {
+            if n_x != 1 && n_y != 1 && grid[[n_x,n_y]] != 0 {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+        if living_neighbours < 2 || living_neighbours > 3 {
+            println!("cell dies");
+            next_grid[[outer_x,outer_y]] = 0;
         }
-      });
-      if living_neighbours < 2 || living_neighbours > 3 {
-	println!("cell dies");
-        next_grid[[outer_x,outer_y]] = 0;
-      }
-      if living_neighbours == 3 {
-        println!("cell lives or reproduces");
-        next_grid[[outer_x,outer_y]] = 1;
-      }
+        if living_neighbours == 3 {
+            println!("cell lives or reproduces");
+            next_grid[[outer_x,outer_y]] = 1;
+        }
     });
 }
