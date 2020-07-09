fa931747ec4609a3bb273baf2719900f056bc155
Vorlent <vorlent@web.de>
2020-07-10 00:23:57 +0200

    Created parallel version

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,15 @@
 use rayon::prelude::*;
-use ndarray::{s, Array2, ArrayView2};
+use ndarray::{s, Array2, ArrayView2, Zip};
 
 fn main() {
     let mut grid = Array2::<i32>::zeros((10, 10));
     let mut next_grid = Array2::<i32>::zeros((10, 10));
 
     let inner_grid = grid.slice(s![1..-1, 1..-1]);
-    inner_grid.indexed_iter().for_each(|((x,y), v)| {
+    let mut inner_next_grid = next_grid.slice_mut(s![1..-1, 1..-1]);
+    Zip::indexed(inner_grid)
+        .and(&mut inner_next_grid)
+        .par_apply(|(x,y), v, nv| {
         let outer_x = x + 1;
         let outer_y = y + 1;
         println!("cell {},{} has value {}", outer_x, outer_y, v);
@@ -20,11 +23,11 @@ fn main() {
         });
         if living_neighbours < 2 || living_neighbours > 3 {
             println!("cell dies");
-            next_grid[[outer_x,outer_y]] = 0;
+            *nv = 0;
         }
         if living_neighbours == 3 {
             println!("cell lives or reproduces");
-            next_grid[[outer_x,outer_y]] = 1;
+            *nv = 1;
         }
     });
 }
