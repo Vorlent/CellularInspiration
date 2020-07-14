c79ebb02759afe78bf02c9dff04747344d5102a9
Vorlent <vorlent@web.de>
2020-07-14 16:42:01 +0200

    Added hopper direction

--- grid_sim_rust/src/main.rs
+++ grid_sim_rust/src/main.rs
@@ -19,10 +19,66 @@ enum ItemType {
 #[derive(Copy, Clone)]
 struct Block {
     block_type: BlockType,
-    item_type: ItemType
+    item_type: ItemType,
+    direction_x: i32,
+    direction_y: i32
 }
 
-const N: usize = 2000;
+impl Block {
+    fn update_neighbour(&self, x: i32, y: i32, nx: i32, ny: i32, cv: &Block, nxtv: &mut Block) -> () {
+        match self.block_type {
+            BlockType::Hopper => {
+                match cv.block_type {
+                    BlockType::Chest => {
+                        match self.item_type {
+                            ItemType::None => {
+                                if(self.direction_x == nx && self.direction_y == ny) {
+                                    println!("1 Transfer from Chest ({},{}) to Hopper!", x, y);
+                                    println!("Chest ({},{}) deletes its item", x, y);
+                                    nxtv.item_type = ItemType::None;
+                                }
+                            },
+                            _ => {
+                                if(self.direction_x != nx || self.direction_y != ny) {
+                                    println!("1 Transfer to Chest ({},{}) from Hopper!", x, y);
+                                    println!("Chest ({},{}) adds item ({:?}) from hopper", x, y, self.item_type);
+                                    nxtv.item_type = self.item_type;
+                                }
+                            }
+                        }
+                    }
+                    _ => ()
+                }
+            },
+            BlockType::Chest => {
+                match cv.block_type {
+                    BlockType::Hopper => {
+                        match self.item_type {
+                            ItemType::None => {
+                                if(self.direction_x == nx && self.direction_y == ny) {
+                                    println!("Transfer to Chest ({},{}) from Hopper!", x, y);
+                                    println!("Hopper ({},{}) deletes its item", x, y);
+                                    nxtv.item_type = ItemType::None;
+                                }
+                            },
+                            _ => {
+                                if(self.direction_x != nx || self.direction_y != ny) {
+                                    println!("Transfer from Chest ({},{}) to Hopper!", x, y);
+                                    println!("Hopper ({},{}) adds item ({:?}) from chest", x, y, self.item_type);
+                                    nxtv.item_type = self.item_type;
+                                }
+                            }
+                        }
+                    },
+                    _ => ()
+                }
+            },
+            _ => ()
+        }
+    }
+}
+
+const N: usize = 200;
 
 // bounded grid coordinate
 fn bgc(coord: i32) -> i32 {
@@ -30,69 +86,23 @@ fn bgc(coord: i32) -> i32 {
 }
 
 fn main() {
-    let empty_block = Block { block_type: BlockType::None, item_type: ItemType::None };
+    let empty_block = Block { block_type: BlockType::None, item_type: ItemType::None, direction_x: 0, direction_y: 0 };
 
     let mut grid = Array2::<Block>::from_elem((N, N), empty_block);
 
-    grid[[3,3]] = Block { block_type: BlockType::Chest, item_type: ItemType::Apple };
-    grid[[3,4]] = Block { block_type: BlockType::Hopper, item_type: ItemType::None };
-    grid[[3,5]] = Block { block_type: BlockType::Chest, item_type: ItemType::None };
+    grid[[3,3]] = Block { block_type: BlockType::Chest, item_type: ItemType::Apple, direction_x: 0, direction_y: 0 };
+    grid[[3,4]] = Block { block_type: BlockType::Hopper, item_type: ItemType::None, direction_x: 3, direction_y: 5 };
+    grid[[3,5]] = Block { block_type: BlockType::Chest, item_type: ItemType::None, direction_x: 0, direction_y: 0 };
 
     let mut next_grid = Array2::<Block>::from_elem((N, N), empty_block);
 
     Zip::indexed(&mut next_grid)
-        .par_apply(|(x,y), nxtv| {
+        .par_apply(|(x,y), mut nxtv| {
             let cv = grid[[x,y]];
+            nxtv.block_type = cv.block_type;
             let neighbours = grid.slice(s![bgc((x as i32)-1)..=bgc((x as i32)+1), bgc((y as i32)-1)..=bgc((y as i32)+1)]);
-            Zip::indexed(neighbours).apply(|(x,y), nv| {
-                nxtv.block_type = cv.block_type;
-                match cv.block_type {
-                    BlockType::Chest => {
-                        match nv.block_type {
-                            BlockType::Hopper => {
-                                match nv.item_type {
-                                    ItemType::None => {
-                                        println!("Transfer from Chest ({},{}) to Hopper!", x, y);
-                                        println!("Chest ({},{}) deletes its item", x, y);
-                                        nxtv.item_type = ItemType::None;
-                                    },
-                                    _ => {
-                                        println!("Transfer to Chest ({},{}) from Hopper!", x, y);
-                                        println!("Chest ({},{}) adds item ({:?}) from hopper", x, y, nv.item_type);
-                                        nxtv.item_type = nv.item_type;
-                                    }
-                                }
-                            },
-                            _ => ()
-                        }
-                    },
-                    BlockType::Hopper => {
-                        match nv.block_type {
-                            BlockType::Chest => {
-                                match nv.item_type {
-                                    ItemType::None => {
-                                        match nv.item_type {
-                                            ItemType::None => {
-                                            },
-                                            _ => {
-                                                println!("Transfer to Chest ({},{}) from Hopper!", x, y);
-                                                println!("Hopper ({},{}) deletes its item", x, y);
-                                                nxtv.item_type = ItemType::None;
-                                            }
-                                        }
-                                    },
-                                    _ => {
-                                        println!("Transfer from Chest ({},{}) to Hopper!", x, y);
-                                        println!("Hopper ({},{}) adds item ({:?}) from chest", x, y, nv.item_type);
-                                        nxtv.item_type = nv.item_type;
-                                    }
-                                }
-                            },
-                            _ => ()
-                        }
-                    },
-                    _ => ()
-                }
+            Zip::indexed(neighbours).apply(|(nx,ny), nv| {
+                nv.update_neighbour((nx as i32) + (x as i32)-1, (ny as i32) + (y as i32)-1, x as i32, y as i32, &cv, &mut nxtv);
             })
     });
 
